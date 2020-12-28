5b776682e1c8c03963522b26cd28ddca76d15255
Lakshay saini <lakshaysaini2013@gmail.com>
2020-12-28 23:53:24 +0530

    User details and update route added

--- backend/routes/User.routes.js
+++ backend/routes/User.routes.js
@@ -3,12 +3,16 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/User.controllers.js";
 import { protect } from "../middlewares/Auth.milddleware.js";
 
 const router = express.Router();
 
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route("/").post(registerUser);
 export default router;
